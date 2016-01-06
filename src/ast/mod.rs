8a1b319fbb654bdf94bacdf7314958585fcc506d
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2016-01-06 16:24:42 +0100

    Add ast module from old project

---
+++ src/ast/mod.rs
@@ -0,0 +1,123 @@
+// TODO: Remove
+#![allow(dead_code)]
+
+use std::fmt::{Display, Formatter, Error};
+use base::code::{BytePos, Span};
+use std::vec::Vec;
+use std::default::Default;
+
+macro_rules! java_enum { (
+    $name:ident { $( $variant:ident => $java_word:expr, )* }
+) => {
+    #[derive(Copy, Clone, PartialEq, Eq, Debug, Hash)]
+    pub enum $name {
+        $( $variant, )*
+    }
+
+    impl $name {
+        pub fn as_java_string(&self) -> &str {
+            match *self {
+                $( $name::$variant => $java_word , )*
+            }
+        }
+    }
+
+    impl Display for $name {
+        fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+            self.as_java_string().fmt(f)
+        }
+    }
+}}
+
+
+// A Java compilation unit: "File that contains one class"
+#[derive(Debug, Clone)]
+pub struct CompilationUnit {
+    pub items: Vec<Item>,
+}
+
+#[derive(Debug, Clone)]
+pub enum Item {
+    Import(Import),
+    Class(Box<Class>),
+    Method(Box<Method>),
+}
+
+
+#[derive(Debug, Clone)]
+pub struct Ident {
+    pub name: String,
+    pub span: Span,
+}
+
+impl Default for Ident {
+    fn default() -> Self {
+        Ident {
+            name: "".into(),
+            span: Span { lo: BytePos(0), hi: BytePos(0) },
+        }
+    }
+}
+
+/// A import declaration
+#[derive(Debug, Clone)]
+pub enum Import {
+    /// e.g. `import IO.AlgoTools;`
+    Single(Name),
+    /// called "type-import-on-demand" in specs -- e.g. `import IO.*;`
+    Wildcard(Name),
+}
+
+#[derive(Debug, Copy, Clone)]
+pub enum Visibility {
+    Public,
+    Protected,
+    Package,
+    Private,
+}
+
+#[derive(Debug, Clone)]
+pub struct Class {
+    pub name: Ident,
+    pub vis: Visibility,
+    pub methods: Vec<Method>,
+}
+
+#[derive(Debug, Clone)]
+pub struct Name {
+    // for qualified names
+    pub path: Vec<Ident>,
+    pub last: Option<Ident>,
+}
+
+java_enum! (Modifier {
+    Public => "public",
+    Protected => "protected",
+    Private => "private",
+    Abstract => "abstract",
+    Static => "static",
+    Final => "final",
+    Synchronized => "synchronized",
+    Native => "native",
+    Strictfp => "strictfp",
+    Transient => "transient",
+    Volatile => "volatile",
+});
+
+#[derive(Debug, Clone)]
+pub struct Method {
+    pub vis: Visibility,
+    pub name: Ident,
+    pub ret_ty: Ident,
+    pub static_: bool,
+    pub final_: bool,
+    pub params: Vec<FormalParameter>,
+}
+
+#[derive(Debug, Clone)]
+pub struct FormalParameter {
+    pub ty: Ident,
+    pub name: Ident,
+    pub dims: usize,
+    pub final_: bool,
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 extern crate xswag_base as base;
 
 pub mod lex;
+pub mod ast;
