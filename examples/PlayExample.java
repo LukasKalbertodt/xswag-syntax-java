75a4d3f16d77553d8967e40266c17f1b36b89152
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2016-02-22 00:44:52 +0100

    Extract example java code into external file to avoid recompiles

---
+++ examples/PlayExample.java
@@ -0,0 +1,7 @@
+import AlgoTools.IO;
+
+public class Cheese {
+    public static void main(String[] args) {
+        int x;
+    }
+}
--- examples/play.rs
+++ examples/play.rs
@@ -1,31 +1,25 @@
 extern crate xswag_syntax_java as syntax;
 extern crate xswag_base as base;
 
+use std::fs::File;
+use std::io::Read;
 use base::diag;
 
-// FIXME: import is currently wrong
-const SRC: &'static str = "\
-import AlgoTools.IO;
-
-public class Cheese {
-    public static void main(String[] args) {
-        int x;
-    }
-}
-
-";
-
 fn main() {
-    let file = base::code::FileMap::new("<test>", SRC);
-    let (res, errors) = syntax::parse_compilation_unit(&file);
+    let mut file = File::open("examples/PlayExample.java").unwrap();
+    let mut src = String::new();
+    file.read_to_string(&mut src).unwrap();
+
+    let file_map = base::code::FileMap::new("PlayExample.java", src);
+    let (res, errors) = syntax::parse_compilation_unit(&file_map);
 
     match res {
         Ok(ast) => println!("{:#?}", ast),
         Err(e) => {
-            diag::print(&e, &file, diag::PrintOptions::default());
+            diag::print(&e, &file_map, diag::PrintOptions::default());
         }
     }
     for e in &errors {
-        diag::print(&e, &file, diag::PrintOptions::default());
+        diag::print(&e, &file_map, diag::PrintOptions::default());
     }
 }
